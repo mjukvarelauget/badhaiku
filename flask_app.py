d0478d5d44c8b3bc49198cd82fc1a9e5bbcee49a
Håvard Opheim <haavard@vaage.com>
2019-10-30 20:58:47 +0100

    Add response on root

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def get_root():
+    return "hjelp!"
+
 @app.route('/bad/haiku', methods=["GET"])
 def get_haiku():
     return jsonify({'haiku': haiku_as_list()})
