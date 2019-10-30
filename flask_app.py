7972db6b70ba4c6b26a0b2254324401473b41ac6
Håvard Opheim <haavard@vaage.com>
2019-10-30 20:05:02 +0100

    Add test API

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from haiku import haiku_as_list
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/bad/haiku', methods=["GET"])
+def get_haiku():
+    return jsonify({'haiku': haiku_as_list()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
